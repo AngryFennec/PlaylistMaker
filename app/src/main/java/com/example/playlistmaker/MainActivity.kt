08b3bf4947c2febd97124ebce2f52e4c06b09ec7
Angry Fennec <shellfix@gmail.com>
2023-09-13 19:37:29 +0300

    Переключение активити

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnSearch = findViewById<Button>(R.id.btnSearch)
+        val btnMedia = findViewById<Button>(R.id.btnMedia)
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+
+        btnSearch.setOnClickListener {
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnMedia.setOnClickListener {
+            val intent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSettings.setOnClickListener {
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }

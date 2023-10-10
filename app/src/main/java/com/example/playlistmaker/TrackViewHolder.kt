52f2a9762246193287f03d403c99e5a2ff4fb3bc
Angry Fennec <shellfix@gmail.com>
2023-10-10 21:48:25 +0300

    отображение списка треков

---
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,30 @@
+package com.example.playlistmaker
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val trackNameTextView: TextView = itemView.findViewById(R.id.track_name)
+    private val artistNameTextView: TextView = itemView.findViewById(R.id.artist_name)
+    private val trackTimeTextView: TextView = itemView.findViewById(R.id.track_time)
+    private val artworkImageView: ImageView = itemView.findViewById(R.id.artwork_url_100)
+
+    fun bind(track: Track) {
+        trackNameTextView.text = track.trackName
+        artistNameTextView.text = track.artistName
+        trackTimeTextView.text = track.trackTime
+
+        Glide.with(itemView)
+            .load(track.artworkUrl100)
+            .error(R.drawable.track_placeholder)
+            .placeholder(R.drawable.track_placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(10))
+            .into(artworkImageView)
+    }
+}
